e72712f04579951a05c562fa36866db9d78d1278
Vivin <vivinmadurai@gmail.com>
2023-12-29 17:31:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,46 +13,77 @@ async function getResponse(prompt) {
   const result = await chat.sendMessage(prompt);
   const response = await result.response;
   const text = response.text();
-  
-console.log(text);
-  return text;
 
+  console.log(text);
+  return text;
 }
 
 // user chat div
 export const userDiv = (data) => {
   return `
-  <!-- User Chat -->
-          <div class="flex items-center gap-2 justify-end">
-            <p class="bg-gemDeep text-white p-1 rounded-md-user shadow-md  ">
-              ${data}
-            </p>
-            <img
-              src="user.jpg"
-              alt="user icon"
-              class="w-10 h-10 rounded-full"
-            />
-          </div>
+    <!-- User Chat -->
+    <div class="flex items-center gap-2 justify-end">
+      <p class="bg-gemDeep text-white p-1 rounded-md-user shadow-md">
+        ${data}
+      </p>
+      <img src="user.jpg" alt="user icon" class="w-10 h-10 rounded-full" />
+    </div>
   `;
 };
 
 // AI Chat div
 export const aiDiv = (data) => {
   return `
-  <!-- AI Chat -->
-          <div class="flex gap-2 justify-start">
-          <img
-              src="https://cdn.hashnode.com/res/hashnode/image/upload/v1679548216146/UVfbFzJqZ.png?auto=compress"
-              alt="user icon"
-              class="w-10 h-10"
-            />
-            <pre class="bg-gemRegular/40 text-gemDeep p-1 rounded-md-ai shadow-md whitespace-pre-wrap">
-              ${data}
-            </pre>
-          </div>
+    <!-- AI Chat -->
+    <div class="flex gap-2 justify-start">
+      <img
+        src="https://cdn.hashnode.com/res/hashnode/image/upload/v1679548216146/UVfbFzJqZ.png?auto=compress"
+        alt="user icon"
+        class="w-10 h-10"
+      />
+      <pre class="bg-gemRegular/40 text-gemDeep p-1 rounded-md-ai shadow-md whitespace-pre-wrap">
+        ${data}
+      </pre>
+    </div>
   `;
 };
 
+// Function to simulate AI typing
+function simulateTyping(callback) {
+  // Show typing indicator
+  showTypingIndicator();
+
+  // Simulate typing logic here
+  setTimeout(function () {
+    // Hide typing indicator
+    hideTypingIndicator();
+
+    // Call the provided callback
+    callback();
+  }, 2000); // Adjust the timeout based on your typing simulation speed
+}
+
+// Function to show the typing indicator
+function showTypingIndicator() {
+  document.getElementById('typing-indicator').style.display = 'block';
+}
+
+// Function to hide the typing indicator
+function hideTypingIndicator() {
+  document.getElementById('typing-indicator').style.display = 'none';
+}
+
+// Function to display user message
+function displayUserMessage(message) {
+  // Display user message logic here
+}
+
+// Function to display bot message
+function displayBotMessage(message) {
+  // Display bot message logic here
+}
+
+// Submit handler
 async function handleSubmit(event) {
   event.preventDefault();
 
@@ -68,29 +99,34 @@ async function handleSubmit(event) {
 
   chatArea.innerHTML += userDiv(prompt);
   userMessage.value = "";
-  const aiResponse = await getResponse(prompt);
-  let md_text = md().render(aiResponse);
-  chatArea.innerHTML += aiDiv(md_text);
 
-  let newUserRole = {
-    role: "user",
-    parts: prompt,
-  };
-  let newAIRole = {
-    role: "model",
-    parts: aiResponse,
-  };
+  // Simulate AI typing and response
+  simulateTyping(async function () {
+    // Display AI response
+    const aiResponse = await getResponse(prompt);
+    let md_text = md().render(aiResponse);
+    chatArea.innerHTML += aiDiv(md_text);
 
-  history.push(newUserRole);
-  history.push(newAIRole);
+    let newUserRole = {
+      role: "user",
+      parts: prompt,
+    };
+    let newAIRole = {
+      role: "model",
+      parts: aiResponse,
+    };
 
-  console.log(history);
+    history.push(newUserRole);
+    history.push(newAIRole);
+
+    console.log(history);
+  });
 }
 
+// Event listeners
 const chatForm = document.getElementById("chat-form");
 chatForm.addEventListener("submit", handleSubmit);
 
 chatForm.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) handleSubmit(event);
 });
-
